9c5aeb645e09aa0bfa14de081c05b44b247c98c6
TheOneTold <74574842+TheOneTold@users.noreply.github.com>
2021-03-23 16:33:45 -0500

    Added Jesse stats

--- Gangweed JRPG/main.cpp
+++ Gangweed JRPG/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <unistd.h>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 
 class Enemy {
@@ -15,7 +17,7 @@ class Ally {
     public:
     string allyName;
     short allyHP;
-    short maxAllyHP;
+    short allyMaxHP;
     char allyMoves[10];
     int allyID;
 };
@@ -43,25 +45,37 @@ int main() {
     johnChapin.enemyMaxHP = 100;
     johnChapin.enemyID = 1;
     johnChapin.enemyMoves[1] = {1};
-    
+
     Enemy kuronbo;
     kuronbo.enemyName = "Kuronbo";
     kuronbo.enemyHP = 100;
     kuronbo.enemyMaxHP = 100;
     kuronbo.enemyID = 2;
     kuronbo.enemyMoves[1] = {1};
-    
+
     Enemy *enemySlots[2] = {&johnChapin,&kuronbo};
 
     Ally jesseDotson;
+    jesseDotson.allyName = "Jesse Dotson";
+    jesseDotson.allyHP = 100;
+    jesseDotson.allyMaxHP = 100;
+    jesseDotson.allyID = 1;
+    jesseDotson.allyMoves[1] = {1};
 
     int selectedMoveID;
     int selectedSlotID;
     while (johnChapin.enemyHP > 0 || kuronbo.enemyHP > 0) {
+        select_move:
         cout << "Move id?" << endl;
         cin >> selectedMoveID;
+        // Checks to see if ally has selected move.
+        bool allyHasMove = std::find(std::begin(jesseDotson.allyMoves), std::end(jesseDotson.allyMoves), selectedMoveID) != std::end(jesseDotson.allyMoves);
+        if (!allyHasMove){
+            cout << "Invalid move." << endl;
+            goto select_move;
+        }
         cout << "Slot id?" << endl;
-        cin >> selectedSlotID;  
+        cin >> selectedSlotID;
         battleMove(selectedMoveID, enemySlots[selectedSlotID]);
         if (johnChapin.enemyHP > 0) {
             cout << johnChapin.enemyName << ": " << johnChapin.enemyHP << endl;
